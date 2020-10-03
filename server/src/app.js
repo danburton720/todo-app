5e1716f394a8b344d924f31ef97259fa275dd638
Dan Burton <danburton720@hotmail.com>
2020-10-03 17:03:57 +0100

    added task routes

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 require('./db/mongoose');
 
 const todoRouter = require('./routers/todo');
+const taskRouter = require('./routers/task');
 
 const app = express();
 
 app.use(express.json());
 app.use(todoRouter);
+app.use(taskRouter);
 
 module.exports = app;
