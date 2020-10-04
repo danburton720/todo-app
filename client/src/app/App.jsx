a04fb90f924ceed83eacf25d9fb0392df79abdd0
Dan Burton <danburton720@hotmail.com>
2020-10-04 11:27:38 +0100

    initial set up of redux actions, reducers, and store, plus start to build containers and components for the app

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import TodosContainer from './containers/TodosContainer';
+
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                Hello World!
+                <TodosContainer />
             </div>
         )
     }
---
+++ client/src/app/containers/TodosContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TodosContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                List of todos
+            </div>
+        )
+    }
+}
